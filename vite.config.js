6326c86da251e1409302aecb058c2795f55003f8
akshay-it23 <it23039@glbitm.ac.in>
2025-10-25 16:14:04 +0530

    vite error

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ak-portfolio/', // ✅ critical (note trailing slash)
+  base: '/', // ✅ critical (note trailing slash)
 });
